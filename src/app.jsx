7d38cbebd26fbb51c95c47e17f2f9c31b19ed8c3
adammmertel <mertel.adam@gmail.com>
2020-05-11 21:00:44 +0200

    Apply basic layout

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,56 @@
-import Button from "./components/button";
+import {
+  Button,
+  Section,
+  Tabs,
+  Hero,
+  Container,
+  Heading,
+} from "react-bulma-components";
 
 import React from "react";
+
+const sections = [
+  { title: "Contact" },
+  { title: "Education" },
+  { title: "Work Experience" },
+  { title: "Skills" },
+  { title: "Projects and studies" },
+];
 
 const App = (props) => {
   return (
     <div>
-      <Button />
+      <Hero color="primary" size="medium">
+        <Hero.Body>
+          <Container>
+            <Heading>Adam Mertel</Heading>
+            <Heading subtitle size={3}>
+              maps, programming and data analysis
+            </Heading>
+          </Container>
+        </Hero.Body>
+        <Hero.Footer>
+          <Tabs type="toggle" fullwidth align="centered">
+            {sections.map((section, si) => {
+              return (
+                <Tabs.Tab key={si} active={si === 0 ? true : false}>
+                  {section.title}
+                </Tabs.Tab>
+              );
+            })}
+          </Tabs>
+        </Hero.Footer>
+      </Hero>
+
+      <Container>
+        {sections.map((section, si) => {
+          return (
+            <Section key={si}>
+              <Heading>{section.title}</Heading>
+            </Section>
+          );
+        })}
+      </Container>
     </div>
   );
 };
--- src/components/button.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { Button } from "react-bulma-components";
-
-export default () => {
-  return <Button color="primary">My button</Button>;
-};
