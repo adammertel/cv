e3145fcffbdc3b9f3d08dd7c2361104a7527594e
adammertel <mertel.adam@gmail.com>
2022-10-16 20:54:27 -0500

    Update work experience

--- src/items/work.js
+++ src/items/work.js
@@ -1,36 +1,40 @@
 var itemsWork = [
-    {
-        time: "Oct 2020 – ",
-        label: "PostDoc, webdeveloper",
-        text1: "Casus Science",
-        link1: "https://www.casus.science/",
-    },
-    {
-        time: "Jan 2019 – Nov 2020",
-        label: "Programmer, GIS expert",
-        text1: "Czech Academy of Science",
-    },
-    {
-        time: "Mar 2018 – Aug 2020",
-        label: "Programmer",
-        text1: "Czech Radio, Analytical team",
-    },
-    {
-        time: "Jan 2019 – ",
-        label: "Geospatial programmer",
-        text1: "Freelancer",
-    },
-    {
-        time: "Aug 2015 – Nov 2020",
-        label: "Researcher",
-        text1: "Masaryk University",
-    },
-    {
-        time: "Apr 2017 – Mar 2019",
-        label: "Programmer",
-        text1:
-            "University of Vienna, Department of Geography and Regional Research",
-    },
+  {
+    time: "Oct 2020 – ",
+    label: "post doc, web developer",
+    text1: "Casus Science",
+    link1: "https://www.casus.science/",
+  },
+  {
+    time: "Jan 2019 – Nov 2020",
+    label: "GIS specialist, data analyst",
+    text1: "Czech Academy of Science",
+    link1: "https://www.avcr.cz/en/",
+  },
+  {
+    time: "Mar 2018 – Aug 2020",
+    label: "web developer",
+    text1: "Czech Radio, Analytical team",
+    link1: "https://english.radio.cz/",
+  },
+  {
+    time: "Jan 2019 – ",
+    label: "geospatial developer",
+    text1: "Freelancer",
+  },
+  {
+    time: "Aug 2015 – Nov 2020",
+    label: "researcher",
+    text1: "Masaryk University",
+    link1: "https://www.muni.cz/en",
+  },
+  {
+    time: "Apr 2017 – Mar 2019",
+    label: "geospatial developer",
+    text1:
+      "University of Vienna, Department of Geography and Regional Research",
+    link1: "https://www.univie.ac.at/en/",
+  },
 ];
 
 export default itemsWork;
